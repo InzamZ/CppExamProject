b1f310d240efd18b872395c2af93a3aa9f4ccfef
InzamZ <zhengyinzhen2001@icloud.com>
2021-04-11 19:57:21 +0800

    add a simple member.h files

---
+++ member.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+
+class member
+{
+protected:
+    enum {HUMANRESOURCES = 0,  FINANCE, SALES, ADMINISTRATION};
+    enum {MANAGER = 0,  FINANCE, SALES, ADMINISTRATION};
+    std::string eid;
+    std::string name;
+    std::string gender;
+    std::string dob;
+    std::string doe;
+    unit myunit;
+    title mytitle;
+    //TODO:add the init value of salary
+    const double salary[] {};
+}
+
+//TODO:finish class unit and kids class
+class unit
+{
+protected:
+
+}
+
+//TODO:finish class title and kids class
+class title
+{
+protected:
+
+}
