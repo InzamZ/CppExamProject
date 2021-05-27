b96f78501ff5ddbf8f0b92b8df25ec4914ecc894
InzamZ <zhengyinzhen2001@icloud.com>
2021-05-27 21:06:17 +0800

    updated

--- Main.cpp
+++ Main.cpp
@@ -1,40 +1,19 @@
 #include <iostream>
+#include <functional>
 #include <fstream>
 #include <string>
 #include "linklist.h"
 #include "employee.h"
 #include "title.h"
+#include "feature.h"
 
 int main()
 {
-    linklist<employee::pointer> Employ;
-    std::ifstream datafile("employee.dat");
-    if (! datafile.is_open()) {
-        std::cout << "Error opening file";
-        exit (1);
-    }
-    std::string eid, name, gender, dob, doe, unitname, titlename;
-    title::value_t sal;
-    title::pointer _title;
-    datafile >> eid >> name >> gender >> dob >> doe >> unitname >> titlename >> sal;
-    do {
-        if (titlename == "Manager")
-            _title = dynamic_cast<title::pointer>(new Manager);
-        if (titlename == "Human_Resource_Staff")
-            _title = dynamic_cast<title::pointer>(new HumanResStaff);
-        if (titlename == "Finance_Staff")
-            _title = dynamic_cast<title::pointer>(new FinanceStaff) ;
-        if (titlename == "Sales_Staff")
-            _title = dynamic_cast<title::pointer>(new SalesStaff) ;
-        if (titlename == "Administration_Staff")
-            _title = dynamic_cast<title::pointer>(new AdminStaff) ;
-        employee::pointer temp = new employee{eid, name, gender, dob, doe, unitname, _title};
-        Employ.push_back(temp);
-        datafile >> eid >> name >> gender >> dob >> doe >> unitname >> titlename >> sal;
-    }
-    while (!datafile.eof());
-    datafile.close();
-    for (auto it = Employ.begin(); it != Employ.end(); ++it)
-        (*it)->infor();
+    sys staffsys;
+    staffsys.welcome();
+    staffsys.loading();
+    staffsys.choose();
+    staffsys.print_all_staff();
+    staffsys.rewrite();
     return 0;
 }
