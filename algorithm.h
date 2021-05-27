0f02761d3164bbce034efe5ae94cd895f47a73fc
InzamZ <zhengyinzhen2001@icloud.com>
2021-05-27 21:05:28 +0800

    add a find template

--- algorithm.h
+++ algorithm.h
@@ -6,3 +6,32 @@
 #include "employee.h"
 #include "title.h"
 
+template <typename T>
+void f()
+{
+    typename T::pointer p;
+}
+/*
+template <typename range, typename compare>
+void sort(range k, compare fn)
+{
+    auto l = k.begin(), r =k.end();
+    for (auto i = l + 1; i != r; ++i) {
+        auto key = (*i);
+        for (auto j = i - 1;j != k.rend() && (*j) > key){
+            s[j + 1] = s[j];
+            j--;
+        }
+        s[j + 1] = key;
+    }
+}*/
+
+template <typename iterator, typename filter>
+iterator find(iterator first, iterator last, filter fn) {
+    for (auto it=first;it!=last;++it)
+    {
+        if (fn(*it)) return it;
+    }
+    return nullptr;
+}
+
